e085a50c67fa057e08290f356872a48a5128d750
Piyush0804-hub <guptapiyush1300@gmail.com>
2026-08-20 11:22:31 +0530

    DAY 6

--- HTTP module/myServer.js
+++ HTTP module/myServer.js
@@ -5,5 +5,5 @@ const server=http.createServer((req,res)=>{
     res.end();
 })
 server.listen(8000, ()=>{
-    console.log("Server is running on port 8000");
+    console.log("Server is running on port 8000 http://localhost:8000/");
 })
