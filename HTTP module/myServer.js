5e9aefb55c4d5d9128ac6b30bfdb43971ea749f4
Piyush0804-hub <guptapiyush1300@gmail.com>
2026-08-27 11:37:35 +0530

    HTTP Module

--- HTTP module/myServer.js
+++ HTTP module/myServer.js
@@ -1,6 +1,8 @@
 //Create my own server using http module
 import http from "http";
 const server=http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("<h1>Hello World</h1>");
     res.write("Welcome to my server");
     res.end();
 })
